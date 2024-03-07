1ffb287c4484cc2a9c89a6d27d26e3af24c79360
n-haruhi <hr23nakanishi@gmail.com>
2024-03-07 09:35:17 +0000

    [Add]tasks/edit作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,6 +29,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
 
   private
 
