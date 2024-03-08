4df0ddc1ef67f09266362310526c254e706d0d2a
n-haruhi <hr23nakanishi@gmail.com>
2024-03-08 06:33:40 +0000

    [Add]tasks/show作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,10 @@ class TasksController < ApplicationController
     end
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def edit
     @task = Task.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
 
-  resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
