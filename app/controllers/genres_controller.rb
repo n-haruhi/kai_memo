965dc3ae763755dc503ecf3dfbe8cb12d6c2fa91
n-haruhi <hr23nakanishi@gmail.com>
2024-03-16 08:11:28 +0000

    [Update]バリデーション、モデル設定

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,10 +1,35 @@
 class GenresController < ApplicationController
   def new
+    @genre = Genre.new
   end
 
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
+
+  def update
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path, notice: "You have updated genre successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "You have deleted genre successfully."
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,10 @@
 class Task < ApplicationRecord
 
   belongs_to :user
+  belongs_to :genre
 
   has_one_attached :task_image
+
+  validates :title, presence: true, uniqueness: true
 
 end
